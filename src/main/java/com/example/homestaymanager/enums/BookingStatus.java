7f12fadc63ee0615c87b0540fb4adb67137e8410
nganngan250903-tech <nganngan250903@gmail.com>
2026-04-23 00:38:48 +0700

    feat:booking và enums of bookingstatus

---
+++ src/main/java/com/example/homestaymanager/enums/BookingStatus.java
@@ -0,0 +1,10 @@
+package com.example.homestaymanager.enums;
+
+public enum BookingStatus {
+    PENDING,
+    CONFIRMED,
+    CHECKED_IN,
+    CHECKED_OUT,
+    CANCELLED,
+    NO_SHOW
+}
---
+++ src/main/java/com/example/homestaymanager/model/Booking.java
@@ -0,0 +1,44 @@
+package com.example.homestaymanager.model;
+
+import com.example.homestaymanager.enums.BookingStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "rooms")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @ManyToOne @JoinColumn(name = "customer_id")
+    private Customer customer;
+    @ManyToOne @JoinColumn(name = "employee_id")
+    private Employee employee;
+    @ManyToOne @JoinColumn(name = "room_id")
+    private LocalDateTime checkIn;
+    private LocalDateTime checkOut;
+    private int guestCount;
+    @Enumerated(EnumType.STRING)
+    private BookingStatus currentStatus;
+    @Column(updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+    private BigDecimal totalAmount;
+    private BigDecimal paidAmount;
+    private boolean hasSentReminder;
+}
